9ccf44bab54352903120681c078708ab1fc89393
rakesh0310 <47220814+rakesh0310@users.noreply.github.com>
2020-07-17 15:37:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
-const port = 8080;
+const port = process.env.PORT;
 const app = express();
 app.use(bodyParser.json());
 
